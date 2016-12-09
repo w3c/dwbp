3f2d4b2f4fe9ec72851c057c266d03af08eae82d
Phil Archer <phila@w3.org>
2016-12-09 20:28:18 +0000

    Getting ready for PR

--- bpconfig.js
+++ bpconfig.js
@@ -1,13 +1,13 @@
 var respecConfig = {
     specStatus: "ED",
     shortName: "dwbp",
-//    publishDate:  "2016-05-16",
-    previousPublishDate: "2015-06-25",
-    previousMaturity: "WD",
-    previousURI: "http://www.w3.org/TR/2015/WD-dwbp-20160112/",
+//    publishDate:  "2016-12-15",
+    previousPublishDate: "2016-08-30",
+    previousMaturity: "CR",
+//    previousURI: "http://www.w3.org/TR/2015/WD-dwbp-20160112/",
     edDraftURI: "http://w3c.github.io/dwbp/bp.html",
     implementationReportURI: "https://www.w3.org/2013/dwbp/wiki/BP_Implementation_Report",
-     lcEnd: "2016-06-12",
+     prEnd: "2017-01-15",
      crEnd: "2016-10-31",
     editors: [
     {
